196ccd5a3a7231de328d4219018c595037489ca1
unknown <dvinysstasytis@gmail.com>
2025-01-31 16:07:17 +0200

    cubit tweaks

--- lib/front/add_transaction_screen/cubit/add_transaction_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
-
-part 'add_transaction_state.dart';
-
-class AddTransactionCubit extends Cubit<AddTransactionState> {
-  AddTransactionCubit() : super(AddTransactionInitial());
-}
---
+++ lib/front/add_transaction_screen/cubit/transaction_form_cubit.dart
@@ -0,0 +1,68 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+import '../../../backend/models/transaction.dart';
+import 'package:pvp_projektas/backend/transaction_repository/transaction_repository.dart';
+
+
+class TransactionFormState extends Equatable {
+  final Transaction? transaction;
+
+  const AddTransactionState({required this.transaction});
+
+  @override
+  List<Object?> get props => [transaction];
+
+  AddTransactionState copyWith({Transaction? transaction}) {
+    return AddTransactionState(
+      transaction: transaction ?? this.transaction,
+    );
+  }
+  }
+
+class AddTransactionCubit extends Cubit<AddTransactionState> {
+  final TransactionRepository transRepository;
+
+  // Initialize with an empty or default transaction
+  AddTransactionCubit(this.transRepository)
+      : super(AddTransactionState(transaction: Transaction(
+    id: 0,
+    title: '',
+    amount: 0.0,
+    isIncome: false,
+    category: '',
+    date: DateTime.now(),
+  )));
+
+
+  // Load a single transaction by ID
+  void loadTransactionById(int id) {
+    // Fetch the transaction from the repository
+    final localTransaction = transRepository.getTransaction(id);
+
+    // Emit the updated state with the loaded transaction
+    emit(state.copyWith(transaction: localTransaction));
+  }
+
+  void addTransaction() {
+    //create local copy of transactions
+    if(state.transaction != null) {
+      Transaction trans = state.transaction!;
+      transRepository.addTransaction(trans);
+      emit(state.copyWith(transaction: trans));
+    }
+  }
+
+  /*void updateTransaction(Transaction transaction) {
+    transRepository.updateTransaction(transaction);
+    loadTransactions(); // Reload transactions after updating
+  }
+
+  void deleteTransaction(int id) {
+    //create local copy of transactions
+    List<Transaction> transactionsList = List.from(state.transactions);
+    transactionsList.removeAt(id);
+    transRepository.deleteTransaction(id);
+    emit(state.copyWith(transactions: transactionsList));
+  }*/
+}
